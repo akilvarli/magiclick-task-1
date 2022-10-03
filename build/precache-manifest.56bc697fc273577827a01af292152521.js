eb96a8b7d9d47c53c2520f45a47625673aebdaec
akilvarli <akilvarli@gmail.com>
2022-10-03 15:45:28 +0300

    deploy for netlify

---
+++ build/precache-manifest.56bc697fc273577827a01af292152521.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7a4fb6342f13a1568db",
+    "url": "/static/css/main.32f8b5af.chunk.css"
+  },
+  {
+    "revision": "c7a4fb6342f13a1568db",
+    "url": "/static/js/main.c7a4fb63.chunk.js"
+  },
+  {
+    "revision": "21c75012907b32587942",
+    "url": "/static/js/1.21c75012.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "352a1e5530ec0886552a30d60751d584",
+    "url": "/static/media/england-flag.352a1e55.png"
+  },
+  {
+    "revision": "f8b5fd2a26ffb7e0518335b629d56336",
+    "url": "/index.html"
+  }
+];
